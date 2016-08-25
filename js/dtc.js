7a7cccfd877c6f8038e693909b93afcf586a04dd
html5css3-pro <html5css3.pro@gmail.com>
2016-08-25 10:28:22 +0300

    d3 fix

--- js/dtc.js
+++ js/dtc.js
@@ -5,7 +5,7 @@ var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 	map = new L.Map('map', {
 		layers: [main],
 		center: [55.7501, 37.6687],
-		zoom: 11 
+		zoom: 11
 	}),
 	overlay = L.tileLayer(osmUrl).addTo(map);
 	overlay.getContainer().style.display = "none";
@@ -16,12 +16,48 @@ var osmUrl = 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 	var marker = L.marker([55.7601, 37.6387]).addTo(map);
 	var marker = L.marker([55.7401, 37.6887]).addTo(map);
 	var marker = L.marker([55.7801, 37.6587]).addTo(map);
-	
+	var changeOpacity = false;
+
 //Convert Leaflet geometries to D3 geometries
 function projectPoint(x, y) {
 	var point = map.latLngToLayerPoint(new L.LatLng(y, x));
 	this.stream.point(point.x, point.y);
 };
+
+function calcLongitudeOffset(radius, latitude) {
+	var ltRadius, earthRadius;
+
+	earthRadius = 6378137.0;
+
+	latitudeRadius = 2 * Math.PI * earthRadius * Math.cos(latitude * 0.0175);
+
+	return 360 * radius / latitudeRadius;
+}
+
+function calcCircleRadius(circle) {
+	var latLng, radius, newLng, centerPoint, edgePoint;
+
+	latLng = circle.getLatLng();
+	radius  = circle.getRadius();
+
+	newLng = latLng.lng + calcLongitudeOffset(radius, latLng.lat);
+	centerPoint = map.latLngToLayerPoint(latLng);
+	edgePoint = map.latLngToLayerPoint(new L.LatLng(latLng.lat, newLng));
+
+	return Math.abs(edgePoint.x - centerPoint.x);
+
+}
+
+function projectCircle(circle)  {
+	var center, cx, cy, r;
+
+	center = map.latLngToLayerPoint(circle.getLatLng());
+	cx = center.x;
+	cy = center.y;
+	r = calcCircleRadius(circle);
+
+	return {cx: cx, cy: cy, r: r};
+}
 
 //Initialize SVG layer in Leaflet (works for leaflet-0.7.3)
 map._initPathRoot()
@@ -82,20 +118,36 @@ var drawControl = new L.Control.Draw({
 map.addControl(drawControl);
 
 map.on('draw:created', function (e) {
-	var type = e.layerType,
+	var geoJSONlayer, type, layer, circle;
+
+	/* Получаем нашу фигурку с гео-координатами */
+	type = e.layerType;
 	layer = e.layer;
 
+	/* Добавляем фигурку в список слоёв */
 	drawnItems.addLayer(layer);
 
-	var geoJSONlayer = layer.toGeoJSON();
-	geoJSONlayer.properties.id = 'l' + layer._leaflet_id;
+	if (type === 'circle') {
+		circle = projectCircle(layer);
 
-	clipPath.append("path")
-	  .attr("id", 'l' + layer._leaflet_id)
-	  .datum(geoJSONlayer)
-	  .attr("d", path)
-	  .attr("fill", "white")
-	  .attr("opacity", 0.95); //change opacity to control masking
+		clipPath.append("circle")
+		  .attr("id", 'l' + layer._leaflet_id)
+		  .attr("cx", circle.cx)
+			.attr("cy", circle.cy)
+			.attr("r", circle.r)
+		  .attr("fill", "white")
+		  .attr("opacity", 0.95); //change opacity to control masking
+	} else {
+		geoJSONlayer = layer.toGeoJSON();
+		geoJSONlayer.properties.id = 'l' + layer._leaflet_id;
+
+		clipPath.append("path")
+		  .attr("id", 'l' + layer._leaflet_id)
+		  .datum(geoJSONlayer)
+		  .attr("d", path)
+		  .attr("fill", "white")
+		  .attr("opacity", 0.95); //change opacity to control masking
+	}
 
 	clip();
 });
@@ -122,13 +174,20 @@ map.on('draw:edited', function (e) {
 
 //Clipping or masking change code accordingly
 function clip() {
-	var clippingPaths =  clipPath.selectAll("path");
+	var clippingPaths =  clipPath.selectAll("path, circle");
 	clippingPaths.attr("d", path);
-	
+
 	if (clippingPaths.size() > 0) {
 		overlay.getContainer().style.display = "inline";
 		overlay.getContainer().style.clipPath = 'url(#clipPath)';
 		overlay.getContainer().style.WebkitClipPath = 'url(#clipPath)';
+		if (changeOpacity === false) {
+			overlay.getContainer().style.opacity = "1";
+			changeOpacity = true;
+		} else {
+			overlay.getContainer().style.opacity = "0.99";
+			changeOpacity = false;
+		}
 	} else {
 		overlay.getContainer().style.display = "none";
 		overlay.getContainer().style.clipPath = 'none';
